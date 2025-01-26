bf09ed6b7d44ff83a52d9ef37e1d904e46dff7e1
saladavide003 <davide.sala@itsrizzoli.it>
2025-01-26 14:12:36 +0100

    Initial commit

---
+++ src/main/java/com/alas/advancedrotation/AdvancedRotation.java
@@ -0,0 +1,16 @@
+package com.alas.advancedrotation;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class AdvancedRotation implements ModInitializer {
+
+	public static final String MOD_ID = "advancedrotation";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/com/alas/advancedrotation/AdvancedRotationClient.java
@@ -0,0 +1,11 @@
+package com.alas.advancedrotation;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AdvancedRotationClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
